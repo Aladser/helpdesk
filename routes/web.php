9791f29beb351c7010754406abda2688a8fd8740
Aladser <aladser@gmail.com>
2024-06-11 11:38:08 +0900

    добавление авторизации

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\TaskController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// индексная страница
+Route::get('/', [TaskController::class, 'index'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
