22b8243c4183b938b5f39d1b50b732ed6fdfbca6
Aladser <aladser@gmail.com>
2024-06-15 11:04:59 +0900

    обновлена верстка

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <p class='text-2xl pb-2'><span class='inline-block w-32 text-xl'>логин:</span> <?php echo $auth_user->login; ?></p>
+                    <p class='text-2xl pb-2'><span class='inline-block w-32 text-xl'>логин:</span> {{$auth_user->login}}</p>
                     <p class='text-2xl pb-2'>
-                        <span class='inline-block w-32 text-xl'>имя:</span> <?php echo "{$auth_user->surname} {$auth_user->name} {$auth_user->patronym}"; ?>
+                        <span class='inline-block w-32 text-xl'>имя:</span> {{$auth_user->full_name()}}
                     </p>
-                    <p class='text-2xl'><span class='inline-block w-32 text-xl'>роль:</span> <?php echo $auth_user->role->description; ?></p>
+                    <p class='text-2xl'><span class='inline-block w-32 text-xl'>роль:</span> {{$auth_user->role->description}}</p>
                 </div>
             </div>
         </div>
