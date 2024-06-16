6f7bd636f1ef926193e0718ece7c70e0ba6d6827
Aladser <aladser@gmail.com>
2024-06-17 07:39:25 +0900

    обновлена отправка комментариев

--- public/js/show.js
+++ public/js/show.js
@@ -1,44 +1,62 @@
 const CSRF_TOKEN = document.querySelector('meta[name="csrf-token"]');
-const TASK_ID = document.querySelector('#task__id').value;
+const TASK_ID = document.querySelector("#task__id").value;
 
-const TASK_NODE = document.querySelector('#task');
-const TASK_STATUS_NODE = document.querySelector('#task__status');
-const TASK_EXECUTOR_NODE = document.querySelector('#task__executor');
+const TASK_NODE = document.querySelector("#task");
+const TASK_STATUS_NODE = document.querySelector("#task__status");
+const TASK_EXECUTOR_NODE = document.querySelector("#task__executor");
 
-const NEW_CMT_FORM_BLOCK = document.querySelector('#new-cmt-form-block');
-const NEW_CMT_FORM = document.querySelector('#new-cmt-form');
-const CMT_LIST_BLOCK = document.querySelector('#cmt-list-block');
+const NEW_CMT_FORM_BLOCK = document.querySelector("#new-cmt-form-block");
+const NEW_CMT_FORM = document.querySelector("#new-cmt-form");
+const NEW_CMT_FORM_MSG_FILED = document.querySelector(
+    "#new-cmt-form__msg-field"
+);
+const CMT_LIST_BLOCK = document.querySelector("#cmt-list-block");
 
-const TASK_BTN_BLOCK = document.querySelector('#task__btn-block');
-let take_task_btn = document.querySelector('#btn-take-task');
-let complete_task_btn = document.querySelector('#btn-complete-task');
+const TASK_BTN_BLOCK = document.querySelector("#task__btn-block");
+let take_task_btn = document.querySelector("#btn-take-task");
+let complete_task_btn = document.querySelector("#btn-complete-task");
 
 //-----взять в работу-----
-if(take_task_btn) {
-    take_task_btn.addEventListener('click', () => sendUpdateTaskStatus(TASK_ID, 'take-task'));
+if (take_task_btn) {
+    take_task_btn.addEventListener("click", () =>
+        sendUpdateTaskStatus(TASK_ID, "take-task")
+    );
 } else {
     //-----выполнить работу-----
-    if(complete_task_btn) {
-        complete_task_btn.addEventListener('click', () => sendUpdateTaskStatus(TASK_ID, 'complete-task'));
+    if (complete_task_btn) {
+        complete_task_btn.addEventListener("click", () =>
+            sendUpdateTaskStatus(TASK_ID, "complete-task")
+        );
     }
 }
 
 // сохранить комментарий------
-NEW_CMT_FORM.addEventListener('submit', function(e){sendStoreComment(e, new FormData(this));});
-document.querySelector("#new-cmt-form__msg-field").addEventListener('keyup', function(e) {
-    if (e.key == 'Enter') {
+NEW_CMT_FORM.addEventListener("submit", function (e) {
+    sendStoreComment(e, new FormData(this));
+});
+// отпускание клавиши Shift и Enter
+let pressed_keys = [];
+NEW_CMT_FORM_MSG_FILED.addEventListener("keyup", function (e) {
+    if (e.key == "Enter" && !pressed_keys.includes("Shift")) {
         let formData = new FormData();
-        formData.append('_token', NEW_CMT_FORM._token.value);
-        formData.append('message', NEW_CMT_FORM.message.value);
-        formData.append('task_id', NEW_CMT_FORM.task_id.value);
+        formData.append("_token", NEW_CMT_FORM._token.value);
+        formData.append("message", NEW_CMT_FORM.message.value);
+        formData.append("task_id", NEW_CMT_FORM.task_id.value);
         sendStoreComment(e, formData);
+    } else if (e.key == "Shift") {
+        pressed_keys.pop();
+    }
+});
+// Нажатие Shift
+NEW_CMT_FORM_MSG_FILED.addEventListener("keydown", function (e) {
+    if (e.key == "Shift") {
+        pressed_keys.push("Shift");
     }
 });
 
-
 // --------- ФУНКЦИИ --------
 /**Обновить статус задачи*/
-function sendUpdateTaskStatus(task_id, action){
+function sendUpdateTaskStatus(task_id, action) {
     let headers = {
         "X-CSRF-TOKEN": CSRF_TOKEN.getAttribute("content"),
         "Content-Type": "application/json",
@@ -49,11 +67,11 @@ function sendUpdateTaskStatus(task_id, action){
     params.id = task_id;
 
     ServerRequest.execute({
-        URL: `/task/${task_id}`, 
-        processFunc: (data) => handleUpdateTask(data), 
-        method: "put", 
-        data: JSON.stringify(params), 
-        headers: headers
+        URL: `/task/${task_id}`,
+        processFunc: (data) => handleUpdateTask(data),
+        method: "put",
+        data: JSON.stringify(params),
+        headers: headers,
     });
 }
 
@@ -62,38 +80,41 @@ function handleUpdateTask(response) {
     let responseData = JSON.parse(response);
 
     if (responseData.is_updated === 1) {
-        if(responseData['action'] == 'take-task') {
+        if (responseData["action"] == "take-task") {
             // взять задачу в работу
 
-            NEW_CMT_FORM_BLOCK.classList.remove('hidden');
+            NEW_CMT_FORM_BLOCK.classList.remove("hidden");
             // исполнитель
             // p id='task__executor' class='mb-2'>Исполнитель: {{$task->executor->full_name()}}</p>
-            let executor_node = document.createElement('p');
-            executor_node.id = 'task__executor';
-            executor_node.className = 'mb-2';
+            let executor_node = document.createElement("p");
+            executor_node.id = "task__executor";
+            executor_node.className = "mb-2";
             executor_node.textContent = `Исполнитель: ${responseData.executor}`;
             TASK_NODE.append(executor_node);
 
             // статус
-            TASK_STATUS_NODE.textContent = 'В работе';
-            TASK_STATUS_NODE.classList.remove('text-rose-600');
-            TASK_STATUS_NODE.classList.add('text-amber-500');
+            TASK_STATUS_NODE.textContent = "В работе";
+            TASK_STATUS_NODE.classList.remove("text-rose-600");
+            TASK_STATUS_NODE.classList.add("text-amber-500");
 
             // кнопка Выполнить
             // <button id='btn-complete-task'class='border px-4 py-2 rounded bg-dark-theme color-light-theme'>Выполнить</button>
-            complete_task_btn = document.createElement('button');
-            complete_task_btn.id = 'btn-complete-task';
-            complete_task_btn.className = 'border px-4 py-2 rounded bg-dark-theme color-light-theme';
-            complete_task_btn.textContent = 'Выполнить';
-            complete_task_btn.addEventListener('click', () => sendUpdateTaskStatus(TASK_ID, 'complete-task'));
+            complete_task_btn = document.createElement("button");
+            complete_task_btn.id = "btn-complete-task";
+            complete_task_btn.className =
+                "border px-4 py-2 rounded bg-dark-theme color-light-theme";
+            complete_task_btn.textContent = "Выполнить";
+            complete_task_btn.addEventListener("click", () =>
+                sendUpdateTaskStatus(TASK_ID, "complete-task")
+            );
             TASK_BTN_BLOCK.removeChild(take_task_btn);
             TASK_BTN_BLOCK.appendChild(complete_task_btn);
-        } else if(responseData['action'] == 'complete-task') {
+        } else if (responseData["action"] == "complete-task") {
             // статус
-            NEW_CMT_FORM_BLOCK.classList.add('hidden');
-            TASK_STATUS_NODE.textContent = 'Выполнена';
-            TASK_STATUS_NODE.classList.remove('text-amber-500');
-            TASK_STATUS_NODE.classList.add('text-green-500');
+            NEW_CMT_FORM_BLOCK.classList.add("hidden");
+            TASK_STATUS_NODE.textContent = "Выполнена";
+            TASK_STATUS_NODE.classList.remove("text-amber-500");
+            TASK_STATUS_NODE.classList.add("text-green-500");
             TASK_BTN_BLOCK.removeChild(complete_task_btn);
         }
     }
@@ -106,7 +127,7 @@ function sendStoreComment(e, formData) {
         URL: "/comment",
         processFunc: (data) => handleStoreComment(data),
         method: "post",
-        data: formData
+        data: formData,
     });
 }
 
@@ -123,8 +144,8 @@ function handleStoreComment(response) {
     */
     let responseData = JSON.parse(response);
     if (response) {
-        let comment_node = document.createElement('div');
-        comment_node.className = 'cmt-list-block__comment';
+        let comment_node = document.createElement("div");
+        comment_node.className = "cmt-list-block__comment";
         comment_node.innerHTML = `
             <div>
                 <div class="cmt-list-block__author color-lighter-theme">${responseData.executor_name}</div>
@@ -133,6 +154,6 @@ function handleStoreComment(response) {
             <div>${responseData.message}</div>
         `;
         CMT_LIST_BLOCK.prepend(comment_node);
-        NEW_CMT_FORM.message.value = '';
+        NEW_CMT_FORM.message.value = "";
     }
 }
