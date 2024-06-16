807a213e0da460ec7be79a80f4f854c0ed53efc7
Aladser <Aladser@users.noreply.github.com>
2024-06-16 14:55:29 +0900

    обновлена тема заголовка

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
-<header class="bg-white shadow">
+<header class="bg-dark-theme color-light-theme shadow-lg shadow-blue-500/40">
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h2 class="font-semibold text-xl leading-tight color-theme">{{ $slot }}</h2>
+        <h2 class="font-semibold text-xl leading-tight">{{ $slot }}</h2>
     </div>
 </header>
