e5201bf62da3cbd6253ffff7805381f84a3dd7fe
Aladser <aladser@gmail.com>
2024-06-21 22:40:15 +0900

    рфкт

--- public/js/request_handlers/StoreCommentHandler.js
+++ public/js/request_handlers/StoreCommentHandler.js
@@ -22,34 +22,36 @@ class StoreCommentHandler{
 
     /**обработать ответ сервера на Сохранить комментарий в БД*/
     handle(response) {
-        /*
-        <div class="cmt-list-block__comment">
-            <div>
-                <div class="cmt-list-block__author text-amber-500">Хохлова А. В.</div>
-                <div class="cmt-list-block__time">2024-06-16 08:56</div>
-            </div>
-            <div>cообщение</div>
-        </div>
-        */
-       
         let response_data = JSON.parse(response);
         if (response_data.is_stored) {
-            let comment_node = document.createElement("div");
-            comment_node.className = "cmt-list-block__comment";
             let author_classname = "cmt-list-block__author ";
-            author_classname +=
-                response_data.author_role == "executor"
-                    ? "color-lighter-theme"
-                    : "text-amber-500";
+            author_classname +=response_data.author_role == "executor" ? "color-lighter-theme": "text-amber-500";
+
+            let comment_node = document.createElement("p");
+            comment_node.className = 'cmt-list-block__comment';
             comment_node.innerHTML = `
-                <div>
-                    <div class="${author_classname}">${response_data.author_name}</div>
-                    <div class="cmt-list-block__time">${response_data.created_at}</div>
-                </div>
-                <div>${response_data.message}</div>
+                    <div>
+                        <div class='cmt-list-block__author ${author_classname}'>${response_data.author_name}</div>
+                        <div class='cmt-list-block__time'>${response_data.created_at}</div>
+                    </div>
+                    <div>${response_data.message}</div>
             `;
             this.comment_list_block.prepend(comment_node);
+            
             this.new_comment_form.message.value = "";
         }
     }
 }
+/*
+<div>
+    @if($comment['role'] == 'executor')
+    <div class='cmt-list-block__author color-lighter-theme'>{{$comment['author_name']}}</div>
+    @else
+    <div class='cmt-list-block__author text-amber-500'>{{$comment['author_name']}}</div>
+    @endif
+
+    <div class='cmt-list-block__time'>
+        {{$comment['created_at']}}
+    </div>
+</div>
+*/
