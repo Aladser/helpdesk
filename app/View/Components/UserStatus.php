fb18b1ebf476328de31720ccb0a3212cdf44437b
Aladser <aladser@gmail.com>
2024-07-02 06:50:55 +0900

    обновлен компонент статуса пользователя

--- app/View/Components/UserStatus.php
+++ app/View/Components/UserStatus.php
@@ -6,21 +6,23 @@ use Illuminate\View\Component;
 
 class UserStatus extends Component
 {
-    /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
+    public $statuses;
+
     public function __construct()
     {
-        //
+        $this->statuses = [];
+        $class_names = ['text-green-400', 'text-orange-400', 'text-red-400'];
+        $user_statuses = \App\Models\UserStatus::all();
+
+        for ($i = 0; $i < count($class_names); ++$i) {
+            $this->statuses[] = [
+                'classname' => $class_names[$i],
+                'name' => $user_statuses[$i]->name,
+                'description' => $user_statuses[$i]->description,
+            ];
+        }
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
-     */
     public function render()
     {
         return view('components.user-status');
